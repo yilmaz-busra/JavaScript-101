3a6d11595da2a4970c8d93620c247c937552fa03
busra <b.busraayilmaz@gmail.com>
2022-08-05 23:54:54 +0300

    JS added

---
+++ js/01-hello-world.js
@@ -0,0 +1,18 @@
+// Merhaba Dünya Örnekleri
+
+// Dokumana Yazdırma İşlemi ile Merhaba Dünya:
+// document.write('Merhaba Dünya') 
+
+// Uyarı Mesajı(Alert) ile Merhaba Dünya:
+// alert("Merhaba Dünya") 
+
+// Uyarı Mesajı(Alert) ile Merhaba Dünya:
+// alert("Merhaba Dünya") 
+
+//console.log ile Merhaba Dünya
+// console.log("Merhaba Dünya") 
+
+alert('Hellooo')
+
+console.log("Hello world")
+document.write('Hello world')
---
+++ js/02-degisken-tanımlama.js
@@ -0,0 +1,41 @@
+ /* LET  VE CONST İLE DEGİSKEN TANIMLAMA  */
+
+// 1 -var ile degisken tanımlama 
+/*  var serverName = "api.kodluyoruz.org"
+ console.log(serverName)*/
+
+ //let ile değiskeni bos tanımlamak
+/*  let serverName ="https://kodluyoruz.org"
+  console.log(serverName)*/
+
+   //let ile değiskene bilgi atayarak tanımlamak
+  /* let password="1234"
+   console.log(password)
+   
+   let fullName="Büşra Yılmaz"
+   console.log(fullName)*/
+
+   //let ile tanımlanan değiskenin içindeki bilgiyi değiştirme 
+   /*fullName="lorem impus"
+   console.log(fullName)*/
+
+   //let ile tanımlanan değiskene bilgi ekleme
+   /*fullName= fullName + "Yeni Bilgi";
+   fullName= " 2:Yeni Bilgi" + fullName;
+   console.log(fullName)
+
+   console.log(fullName + "Test bilgisi")
+   fullname += " Yeni Bilgi" */
+
+   //const ile değişkeni boş tanımlamaya çalışma
+    //const serverPassword; //sadece degisken tanımlandı ama içi boş
+
+   // const ile degisken tanımlamak
+//    const Server_PASSWORD="şlşdfgşdflmhdflzbö"
+//    console.log(Server_PASSWORD);
+
+const Server_PASSWORD="5454545"
+   console.log(Server_PASSWORD);
+
+
+
