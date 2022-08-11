6b89012af0829f47a1f78f47d6367f69700facde
busra <b.busraayilmaz@gmail.com>
2022-08-11 18:03:19 +0300

    added

--- js/ternary_oerator.js
+++ js/ternary_oerator.js
@@ -5,3 +5,38 @@ let info = document.querySelector("#info")
 //ternary kullanımı 
 // kosul ? dogruysa :yanlıssa
 info.innerHTML=` ${userName ? userName:"Kullanıcı ismi girmedi"} `
+
+//TEST SORULARI DENEME
+//soru 1:
+// let boolean;
+
+// const isBooleanTrue = boolean ? true : false;
+
+// console.log(isBooleanTrue)
+// >> false
+
+// console.log(boolean)
+
+//soru2
+// let age = 24;
+// let permission = (age >= 18) ? "Giriş yapabilirsiniz." : "Giriş yapamazsınız.";
+
+// console.log(permission);
+
+// soru3
+// let firstCity = "Ankara";
+
+// let secondCity = "İstanbul";
+
+// const isEqual = (firstCity === secondCity) ? "İki şehir aynı" : "İki şehir aynı değil";
+
+// console.log(isEqual)
+
+// soru4
+// var money;
+// var canBuy = 
+//     (money < 17) ? "Satın alamazsın..":
+//     (money > 30) ? "Satın alabilirsin..":
+//     "Para miktarını girmen gerekmektedir..";
+
+// console.log(canBuy) 
