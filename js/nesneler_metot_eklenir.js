4fb60e3bb79d018e3095800de2658e343f387107
busra <b.busraayilmaz@gmail.com>
2022-09-01 01:35:20 +0300

    added

---
+++ js/nesneler_metot_eklenir.js
@@ -0,0 +1,12 @@
+     let user1= {
+        firstName:"Bilgin",
+        lastName:"Uzman",
+        score: [1, 2, 3, 4],
+        isActive:true,
+        shortName: function() {
+            return`${this.firstName[0].toUpperCase}. ${this.lastName}`
+        }
+    };
+
+    console.log(user1)
+
